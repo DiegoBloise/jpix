5c142e47ec80fa88ea728a67bfce7ce77efde9dc
Diego Bloise <diego-dev@outlook.com>
2024-03-22 00:10:05 -0300

    update: main

--- jix/src/main/java/com/dbl/Main.java
+++ jix/src/main/java/com/dbl/Main.java
@@ -2,6 +2,7 @@ package com.dbl;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(new Jix("Nome", "chavepix", "1.00", "Sao Paulo", "NomeLoja").gerarPayload());
         // CPF: 44455566699
         // CEL: +5511977778888
         // EMAIL: email.exemplo@email.com
